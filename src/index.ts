4679a0c75d6b7187ac00fdd1f6751f2080291db4
Rodrigues Costa <jose@rodriguescosta.com>
2022-06-05 10:22:17 -0300

    feat: add express

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
+import express from 'express'
 
-const a:string = 'Typescript'
+const server = express()
 
-console.log(a)
+server.get('/', (req, res) =>{
+  res.send('Hello World')
+})
+
+
+server.listen(3333, () => {
+  console.log('Server started on port 3333')
+  })
